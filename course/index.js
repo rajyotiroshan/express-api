38d61dde48a3f8a53dd1d6037eca0ab13f708cc7
rajan <rajyotiroshan@gmail.com>
2023-07-19 04:03:14 +0530

    vanilla http server

---
+++ course/index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+const server = http.createServer(
+  /*async*/ (req, res) => {
+    /*     console.log("req");
+    console.log(req);
+    console.log(res);
+    console.log(res); */
+    console.log("Hello from server");
+    if (req.method === "GET" && req.url === "/favicon.ico") {
+      //console.log("fetching favicon");
+      res.statusCode = 204;
+      res.end();
+    }
+    if (req.method === "GET" && req.url === "/") {
+      // console.log("fetching main");
+      res.statusCode = 200;
+      res.end();
+    }
+  }
+);
+
+server.listen(3001, () => {
+  console.log("server on http://localhost:3001");
+});
