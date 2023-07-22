3edd2c375b890434cd0d42a69fc8edaf75190f1e
rajan <rajyotiroshan@gmail.com>
2023-07-22 18:47:03 +0530

    jwt implementation

--- course/src/modules/auth.ts
+++ course/src/modules/auth.ts
@@ -1,4 +1,13 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const comparePasswords = (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
+
+export const hashPassword = (password) => {
+  return bcrypt.hash(password, 5);
+};
 
 export const createJWT = (user) => {
   const token = jwt.sign(
@@ -7,4 +16,34 @@ export const createJWT = (user) => {
   ); //@r a string
 
   return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+  console.log(bearer);
+
+  if (!bearer) {
+    //no access
+    res.status(401);
+    res.json({ message: "not authorized" });
+  }
+
+  const [, token] = bearer.split(" "); // "Bearer sgsgsgsgsgsgsgs"
+
+  if (!token) {
+    res.status(401); // not authorized
+    res.json({ message: "not valid token" });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+
+  res.json({ msg: "no Bearer" });
 };
