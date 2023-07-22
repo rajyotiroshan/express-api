e35596c1eccd61e1fc7f9037cdef218facbeae63
rajan <rajyotiroshan@gmail.com>
2023-07-22 23:28:20 +0530

    signin handler

--- course/src/handlers/user.ts
+++ course/src/handlers/user.ts
@@ -1,14 +1,44 @@
 import prisma from "../modules/db";
-import { createJWT, hashPassword } from "../modules/auth";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
-export const createUser = async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
+export const createNewUser = async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: await hashPassword(req.body.password),
+      },
+    });
+
+    const token = createJWT(user);
+
+    res.json({ token });
+  } catch (err) {
+    res.status(401);
+    res.json(err);
+    return;
+  }
+};
+
+export const signin = async (req, res) => {
+  //find the user with username
+  const user = await prisma.user.findUnique({
+    where: {
       username: req.body.username,
-      password: await hashPassword(req.body.password),
     },
   });
 
+  const isValid = await comparePasswords(req.body.password, user.password);
+
+  if (!isValid) {
+    res.status(401);
+    res.json({ msg: "Unauthorized" });
+    return;
+  }
+
   const token = createJWT(user);
+  res.cookie("auth-", token, {
+    maxAge: 3600000,
+  });
   res.json({ token });
 };
