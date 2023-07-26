f3e7b9959af95506f325160bf750a1ab923113a1
rajan <rajyotiroshan@gmail.com>
2023-07-26 23:14:32 +0530

    update handler

---
+++ course/src/handlers/updates.ts
@@ -0,0 +1,114 @@
+import prisma from "../modules/db";
+
+/**
+ * Handlers for updates (update model)
+ */
+
+/**
+ * @url get "/api/product/:id"
+ * @param req
+ * @param res
+ */
+export const getOneUpdate = async (req, res) => {
+  const updatedData = await prisma.update.findUnique({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.json({ data: updatedData });
+};
+
+/**
+ * @url get "/api/updates"
+ * @param req
+ * @param res
+ * @return "all updates for a user" / "all updates for a product"
+ */
+
+export const getUpdates = async (req, res) => {
+  //get all products for a user
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      updates: true,
+    },
+  });
+
+  const updates = products.reduce((allupdates, products) => {
+    return [...allupdates, products.updates];
+  }, []);
+  res.json({ data: updates });
+};
+
+/**
+ * @url post "/api/update/" body {prodId}
+ * @param req
+ * @param res
+ */
+export const createUpdate = async (req, res) => {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: req.body.id,
+    },
+  });
+  if (!product) {
+    return res.json({ msg: "nope" });
+  }
+
+  const update = await prisma.update.create({
+    data: req.body,
+  });
+
+  res.json({ data: update });
+};
+
+/**
+ * @url put /api/update/:updateId
+ * @param req
+ * @param res
+ */
+export const updateUpdate = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      updates: true,
+    },
+  });
+  const updates = products.reduce((allupdates, product) => {
+    return [...allupdates, ...product.updates];
+  }, []);
+  const match = updates.find((update) => update.id === req.params.id);
+
+  if (!match) {
+    return res.json({ data: "nope" });
+  }
+
+  const updateUpdate = await prisma.update.update({
+    where: {
+      id: req.params.id,
+    },
+    data: req.body,
+  });
+
+  res.json({ data: updateUpdate });
+};
+
+/**
+ * @url delete "/api/update/:updateid
+ * @param req 
+ * @param res 
+ */
+export const deleteAnUpdate = async (req, res) => {
+  const deletedUpdate = await prisma.update.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.json({ data: deletedUpdate });
+};
