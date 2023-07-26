ac0e1f945903ec865ceb19f8d82f7959f57b2cd8
rajan <rajyotiroshan@gmail.com>
2023-07-26 17:19:38 +0530

    products handlers

---
+++ course/src/handlers/products.ts
@@ -0,0 +1,100 @@
+import prisma from "../modules/db"; //prisma client
+
+/**
+ * @url "/api/products"
+ * @param req
+ * @param res
+ * @returns 'All products for a user'
+ *
+ */
+export const getProducts = async (req, res) => {
+  //query for all the products for a user
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      products: true,
+    },
+  });
+
+  res.json({ data: user.products }); //better to use this than just res.json(user.product)
+};
+
+/**
+ * @url "/api/product/:id"
+ * @param req
+ * @param res
+ */
+export const getOneProduct = async (req, res) => {
+  //urlencoded middleware turing all params into an obj assigned to params ans attached to the req obj
+  const id = req.params.id;
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+      belongsTo: req.user.id,
+    },
+  });
+
+  res.json({ data: product });
+};
+
+/**
+ * @url post /api/product
+ * @param req
+ * @param res
+ * @return "created product obj"
+ */
+export const createProduct = async (req, res) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      belongsToId: req.user.id,
+    },
+  });
+
+  res.json({ data: product });
+};
+
+/**
+ * @url put "api/product/:id"
+ * @param req
+ * @param res
+ */
+export const updateProduct = async (req, res) => {
+  const updated = await prisma.product.update({
+    where: {
+      id: req.params.id,
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+    data: {
+      name: req.body.name,
+    },
+  });
+
+  res.json({ data: updated });
+};
+
+/**
+ * @url delete "api/product/:id"
+ * @param req
+ * @param res
+ */
+export const deleteProduct = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id: req.params.id,
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+  });
+
+  res.json({ data: deleted });
+};
