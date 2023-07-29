aa9c5a2d664f4612ddf603fa1b3246e5dc16dfd7
rajan <rajyotiroshan@gmail.com>
2023-07-29 16:33:06 +0530

    unit test demo

---
+++ course/src/handlers/__tests__/user.test.ts
@@ -0,0 +1,11 @@
+describe("user habdler", () => {
+  it("1 is 1", async () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe("user habdler # 2", () => {
+    it("2 is '2' ", async () => {
+      expect(2).toBe(2);
+    });
+  });
